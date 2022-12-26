3c050680241751854058887790a596c136c67983
Douglas Mendes <lixao_9@hotmail.com>
2022-12-26 15:31:10 -0300

    feat: start project spring boot

---
+++ src/main/java/br/com/swa/spring3web/Spring3webApplication.java
@@ -0,0 +1,13 @@
+package br.com.swa.spring3web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring3webApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring3webApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/swa/spring3web/Spring3webApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.swa.spring3web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3webApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
