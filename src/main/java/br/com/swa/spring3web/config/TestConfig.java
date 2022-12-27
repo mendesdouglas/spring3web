e687ec54547f4dc78a54478a85a158caaccc03df
Douglas Mendes <lixao_9@hotmail.com>
2022-12-27 09:44:10 -0300

    feat: implementing enum and orderstatus converter

--- src/main/java/br/com/swa/spring3web/config/TestConfig.java
+++ src/main/java/br/com/swa/spring3web/config/TestConfig.java
@@ -2,6 +2,7 @@ package br.com.swa.spring3web.config;
 
 import br.com.swa.spring3web.entities.Order;
 import br.com.swa.spring3web.entities.User;
+import br.com.swa.spring3web.entities.enums.OrderStatus;
 import br.com.swa.spring3web.repositories.OrderRepository;
 import br.com.swa.spring3web.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Douglas Mendes", "douglas@gmail.com", "988883838338", "123456");
         User u2 = new User(null, "Fegnando Martines", "fernando@gmail.com", "983838383","123456");
 
-        Order o1 = new Order(null, Instant.parse("2022-06-20T19:50:07Z"),u1);
-        Order o2 = new Order(null, Instant.parse("2022-06-20T22:33:07Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2022-06-21T00:40:06Z"),u1);
+        Order o1 = new Order(null, Instant.parse("2022-06-20T19:50:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2022-06-20T22:33:07Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2022-06-21T00:40:06Z"),OrderStatus.WAITING_PAYMENT,u1);
         userRepository.saveAll(Arrays.asList(u1,u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
     }
