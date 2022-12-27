34e008416726a4ec71edf6df813a7f8871db4b77
Douglas Mendes <lixao_9@hotmail.com>
2022-12-27 10:50:36 -0300

    feat: implementing relation manytomany table

--- src/main/java/br/com/swa/spring3web/config/TestConfig.java
+++ src/main/java/br/com/swa/spring3web/config/TestConfig.java
@@ -50,6 +50,15 @@ public class TestConfig implements CommandLineRunner {
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+
         User u1 = new User(null, "Douglas Mendes", "douglas@gmail.com", "988883838338", "123456");
         User u2 = new User(null, "Fegnando Martines", "fernando@gmail.com", "983838383","123456");
 
