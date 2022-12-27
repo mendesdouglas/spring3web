a82ff3b32d66d99dcc204add630d6c3be3e9b1b4
Douglas Mendes <lixao_9@hotmail.com>
2022-12-27 11:49:56 -0300

    Product Order Item one to many

--- src/main/java/br/com/swa/spring3web/entities/Product.java
+++ src/main/java/br/com/swa/spring3web/entities/Product.java
@@ -1,5 +1,6 @@
 package br.com.swa.spring3web.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -26,6 +27,8 @@ public class Product implements Serializable {
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>(); //Set is a interface and cannot be instantiated, hashset is a class implementation of Set
 
+    @OneToMany(mappedBy = "id.product")
+    private  Set<OrderItem> items = new HashSet<>();
     public Product() {
     }
 
@@ -80,6 +83,17 @@ public class Product implements Serializable {
     public Set<Category> getCategories() {
         return categories;
     }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x: items){
+            set.add(x.getOrder());
+        }
+        return set;
+    }
+
+
 
     @Override
     public boolean equals(Object o) {
